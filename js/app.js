5dc295c6ea7a90894df49cc3ba3aa9460698b011
Kimberly Hanson <kimberlyhanson@KimAir13.local>
2017-01-27 10:22:34 -0500

    working

--- js/app.js
+++ js/app.js
@@ -1,36 +1,41 @@
 const app = angular.module('MoviesApp', []);
 // constructor to create new movies
 
-// function Movie(name, release, poster) {
-//     this.name = name;
-//     this.release = release;
-//     this.poster = poster;
-//     this.genre = genre;
-//     this.liked = '';
+function Movie(name, release, poster, overview) {
+    this.title = name;
+    this.release_date = release;
+    this.poster_path = poster;
+    this.overview = overview;
+    this.rated = false;
+    this.rating = null;
 
-//     return this;
-// }
+    return this;
+}
 
 app.factory('MovieService', function ($http) {
     let movies = [];
     console.log(movies);
 
-    
-        $http.get("https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=cbb32409948b71c890e365ef60a341c1").then(function (response){
-        console.log(response);
-        // let profile = [response.data.results.title, response.data.results.release_date, response.data.results.poster_path];
-        // console.log(profile);
-        angular.copy(response.data.results, movies);
-        
 
-        
+    $http.get("https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=cbb32409948b71c890e365ef60a341c1").then(function (response) {
+        console.log(response);
+
+        for (let i = 0; i < response.data.results.length; i++) {
+            let item = response.data.results;
+            item[i] = new Movie(item[i].title, item[i].release_date, item[i].poster_path, item[i].overview);
+
+        }
+        angular.copy(response.data.results, movies);
+
+
+
     })
 
     return {
 
-        addMovie (entry) {
-            movies.push(entry);     
-            
+        addMovie(entry) {
+            movies.push(entry);
+
         },
         getMovies() {
             return movies;
@@ -40,7 +45,7 @@ app.factory('MovieService', function ($http) {
 
         // },
 
-        setRating(film, number){
+        makeRating(film, number) {
             film.stars = number;
             film.isRated = true;
         }
@@ -50,20 +55,20 @@ app.factory('MovieService', function ($http) {
 
 app.controller("SubmitMovieController", function ($scope, MovieService) {
 
-        $scope.name = '';
-        $scope.release = '';
-        $scope.genre = '';
-   
-         $scope.submitMovie = function () {
-       
+    $scope.name = '';
+    $scope.release = '';
+    $scope.genre = '';
+
+    $scope.submitMovie = function () {
+
         const brandNewMovie = new Movie($scope.name, $scope.release, $scope.genre);
-     
+
         MovieService.addMovie(brandNewMovie);
         // clearing the DOM
         $scope.name = '';
         $scope.release = '';
         $scope.genre = '';
-        
+
     };
 
 });
@@ -74,19 +79,23 @@ app.controller('ListOfMoviesController', function ($scope, MovieService) {
     $scope.movies = MovieService.getMovies();
     console.log($scope.movies);
 
-    $scope.likeMovie = function (film) {
-        console.log('liked it');
-        film.liked = 'Good Movie';
-        // have to call this in the factory if create the contructor up there
-        // see favoriteBook function from code today
+    $scope.setRating = function (film, number) {
+        MovieService.makeRating(film, number);
+    }
+
+    // $scope.likeMovie = function (film) {
+    //     console.log('liked it');
+    //     film.liked = 'Good Movie';
+    //     // have to call this in the factory if create the contructor up there
+    //     // see favoriteBook function from code today
 
 
-    };
+    // };
 
-    $scope.noLikeMovie = function (film) {
-        console.log('no likey');
-        film.liked = 'Unwatchable Movie';
-    };
+    // $scope.noLikeMovie = function (film) {
+    //     console.log('no likey');
+    //     film.liked = 'Unwatchable Movie';
+    // };
 
 });
 
